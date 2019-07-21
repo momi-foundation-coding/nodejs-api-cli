3c2d5ff594458ebe6c82d2b1d91ae593c541957c
me-x-mi <ezrqnkemboi@gmail.com>
2019-07-21 17:45:37 +0300

    kemboi cli can create folders and files

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-[![Build Status](https://travis-ci.org/kemboijs/kemboijs-generator.svg?branch=master)](https://travis-ci.org/kemboijs/kemboijs-generator)
+[![Build Status](https://travis-ci.org/kemboijs/kemboijs-cli.svg?branch=master)](https://travis-ci.org/kemboijs/kemboijs-cli)
 [![Maintainability](https://api.codeclimate.com/v1/badges/e6c0ab94bd05adbe3b04/maintainability)](https://codeclimate.com/github/kemboijs/kemboijs-generator/maintainability)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/e6c0ab94bd05adbe3b04/test_coverage)](https://codeclimate.com/github/kemboijs/kemboijs-generator/test_coverage)
+[![Coverage Status](https://coveralls.io/repos/github/kemboijs/kemboijs-cli/badge.svg?branch=master)](https://coveralls.io/github/kemboijs/kemboijs-cli?branch=master)
 
 # kemboijs-generator
 This is a scaffold for generating new application for kemboijs (API based NodeJS framework)
 
 # How to use
 
-- Install Globally `npm i -g create-kemboijs-app`
-- Create app `create-kemboijs-app myfirstapp`
+- Install Globally `npm i -g kemboijs-app`
+- Create app `kemboijs-app myfirstapp`
 
 # Contributors
 
--- tasks/createapp.js
+++ tasks/createapp.js
@@ -1,8 +1,39 @@
 var fs = require("fs");
-var path = require("path");
 
-function createApp(name) {
-    return name;
-}
+// Package file data.
+// const packageJson = {
+//     "name": "My App",
+//     "version": "1.0.0",
+//     "main": "index.js",
+//     "scripts": {
+//         "start": "node ./src/index.js",
+//         "test": "run test"
+//     }
+// }
 
+// Creating the main application
+function createApp(name, dir) {
+    if (!name) {
+        console.log('------You must add the name of your application-------');
+    } else {
+        const foldersToAdd = ['src/controllers', 'src/routes', 'src/middlewares', 'src/config', 'src/models', 'test']
+        const folders = foldersToAdd.map(folder => `${name}/${folder}`);
+
+        folders.forEach(function (folder) {
+            fs.mkdir(folder, { recursive: true }, (err) => {
+                if (err) throw err
+                fs.createWriteStream(`${name}/package.json`);
+                fs.createWriteStream(`${name}/.gitignore`);
+                fs.createWriteStream(`${name}/.env`);
+                fs.createWriteStream(`${name}/README.md`);
+                const files = `${folder}/index.js`;
+                fs.createWriteStream(files)
+                console.log(`----------created file----- ${files}------`);
+            });
+        });
+    }
+};
+
+// Convert into a module that can be used as CLI
 exports = module.exports = createApp;
+// createApp("kemboi")
