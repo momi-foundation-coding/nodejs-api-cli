ef7c983d21cafc2f10339487081537d881f4f138
me-x-mi <ezrqnkemboi@gmail.com>
2019-11-07 11:46:10 +0300

    add console color codes - this PR will go towards closing #35

---
+++ bin/consolecolors.js
@@ -0,0 +1,21 @@
+/**
+ * This file contains colors for console.log. 
+ * This can be setting console log error, warnings or success
+ */
+const colorSet = require("./colorsets");
+
+/**
+ * Set the text to the color passed
+ * Also, it sets reset here
+ */
+const colorString = (color, string) => {
+    // Return the color for the console but reset color
+    return `${color}${string}${colorSet.reset}`
+}
+
+// Console log your chosen color and string
+const chooseYourColorText = (color, string) => {
+    console.log(colorString(color, string))
+}
+
+exports = module.exports = chooseYourColorText;
