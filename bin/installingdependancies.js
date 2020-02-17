9843861a5289cec93173a0a34d7fc52a280fc740
me-x-mi <ezrqnkemboi@gmail.com>
2020-02-17 14:03:58 +0300

    run shell commands one by one
    - fixes issue #13

--- bin/installingdependancies.js
+++ bin/installingdependancies.js
@@ -27,42 +27,65 @@ const installingDependancies = (details) => {
         databaseAdapter = "pg"
     }
 
-    // For now, shell command is run inside this file, but need to be removed later
-    const cmd = `
-        cd ${appBaseDirectory}
-        set -e 
-        echo setting up git for you application
-        git init
-        echo Will install your dependancies shortly....
-        echo We are installing dependancies, please wait...
-        npm install ${framework.toLowerCase()} 
-        npm install body-parser 
-        npm install cors 
-       ${ orm && orm !== 'No ORM' ? `npm install ${orm.toLowerCase()}` : ''}
-        ${databaseAdapter ? `npm install ${databaseAdapter}` : ''}
-        npm install dotenv
-        npm install bcrypt
-        echo Install dev dependancies, please wait...
-        npm install -D @babel/cli 
-        npm install -D @babel/core 
-        npm install -D @babel/node 
-        npm install -D @babel/preset-env 
-        npm install -D chai-http 
-        ${expectationLibrary ? `npm install -D ${expectationLibrary.toLowerCase()}` : ''}
-        ${testFramework ? `npm install -D ${testFramework.toLowerCase()}` : ''} 
-        npm install -D nyc
-        echo adding files to git and committing
-        git add . && git commit -m "create app and set up app"
-    `
-
-    // Install dependancies and dev dependancies
-    exec(cmd, (error, stdout, stderror) => {
-        if (error) {
-            chooseConsoleColorText(colorSet.error, `exec error: ${exec}`);
-        }
-        chooseConsoleColorText(colorSet.normal, `stdout: ${stdout}`);
-        chooseConsoleColorText(colorSet.error, `stderror: ${stderror}`);
-        chooseConsoleColorText(colorSet.log, '------------Thanks for being patient-------------');
+    /**
+     * let commands be in an array 
+     * then, loop through each array 
+     * install each dependancy one by one
+     */
+    let cdBaseDirectory = `cd ${appBaseDirectory}`
+    const commands = [
+        `${cdBaseDirectory}`,
+        `set -e`,
+        `echo Will install your dependancies shortly....`,
+        `echo We are installing dependancies, please wait...`,
+        `${cdBaseDirectory} && npm install ${framework.toLowerCase()}`,
+        `${cdBaseDirectory} && npm install body-parser`,
+        `${cdBaseDirectory} && npm install cors`,
+        `${cdBaseDirectory} && ${orm && orm !== 'No ORM' ? `npm install ${orm.toLowerCase()}` : ''}`,
+        `${cdBaseDirectory} && ${databaseAdapter ? `npm install ${databaseAdapter}` : ''}`,
+        `${cdBaseDirectory} && npm install dotenv`,
+        `${cdBaseDirectory} && npm install bcrypt`,
+        `echo Starting to install dev dependancies, please wait...`,
+        `${cdBaseDirectory} && npm install -D @babel/cli`,
+        `${cdBaseDirectory} && npm install -D @babel/core`,
+        `${cdBaseDirectory} && npm install -D @babel/node`,
+        `${cdBaseDirectory} && npm install -D @babel/preset-env`,
+        `${cdBaseDirectory} && npm install -D chai-http`,
+        `${cdBaseDirectory} && ${expectationLibrary ? `npm install -D ${expectationLibrary.toLowerCase()}` : ''}`,
+        `${cdBaseDirectory} && ${testFramework ? `npm install -D ${testFramework.toLowerCase()}` : ''}`,
+        `${cdBaseDirectory} && npm install -D nyc`,
+        `echo Initializing git for you application`,
+        `${cdBaseDirectory} && git init`,
+        `echo adding folders and files changes`,
+        /**
+         * it seems it initialize the repo, add the first files such as .babelrc, readme.md etc 
+         * except folders src/, test/ and file .lock. 
+         * it also commits the first files
+         * User can commit the changes, but might look into this later.
+         */
+        `${cdBaseDirectory} && git add .`,
+        `echo committing your changes`,
+        `${cdBaseDirectory} && git commit -m "create nodejs-api starter app"`
+    ]
+    /**
+     * run each command and return its response
+     */
+    commands.forEach(command => {
+        exec(command, (error, stdout, stderr) => {
+            chooseConsoleColorText(
+                colorSet.normal,
+                `We are still in the process of installation .......`
+            );
+            if (error) {
+                chooseConsoleColorText(colorSet.error, `exec error: ${exec}`);
+                return;
+            }
+            if (stderr) {
+                chooseConsoleColorText(colorSet.error, `stderror: ${stderr}`);
+                return;
+            }
+            chooseConsoleColorText(colorSet.normal, `stdout: ${stdout}`);
+        });
     });
 }
 
